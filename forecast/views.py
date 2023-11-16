737a714631c323afd5e21159a9b8b9e0ffcc2516
Wonji Lee <w1024ji@gmail.com>
2023-11-16 22:57:53 +0900

    [1116]T1H, SKY, PTY만 필터링해서 가져오기

--- forecast/views.py
+++ forecast/views.py
@@ -4,27 +4,59 @@ from django.shortcuts import render
 from .services import get_weather_data
 from .models import Weather
 import os
+from datetime import datetime, timedelta, date
+
 
 def fetch_and_save_weather(request):
     api_url = 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst'
     api_key = os.getenv('API_KEY', '')
 
+    now = datetime.now()
+    today = datetime.today().strftime("%Y%m%d")
+    y = date.today() - timedelta(days=1)
+    yesterday = y.strftime("%Y%m%d")
+    
+    if now.minute<45: # base_time와 base_date 구하는 함수
+        if now.hour==0:
+            base_time = "2330"
+            base_date = yesterday
+        else:
+            pre_hour = now.hour-1
+            if pre_hour<10:
+                base_time = "0" + str(pre_hour) + "30"
+            else:
+                base_time = str(pre_hour) + "30"
+            base_date = today
+    else:
+        if now.hour < 10:
+            base_time = "0" + str(now.hour) + "30"
+        else:
+            base_time = str(now.hour) + "30"
+        base_date = today
+
+    # print(base_time) # 성공
+
     params = {
-        'numOfRows': 10,
+        'numOfRows': 40,
         'dataType': 'JSON',
         'pageNo': 1,
-        'base_date': '20231116',
-        'base_time': '2035',
+        'base_date': base_date,
+        'base_time': base_time,
         'nx': 61,
         'ny': 128,
     }
 
-    # Fetch data from the API
+    
     response = get_weather_data(api_url, api_key, params)
 
+    # print(response) # 성공
+
+    
     if response and 'response' in response and 'body' in response['response']:
         
         items = response['response']['body']['items']['item']
+
+       
 
         if items:
             for item in items:
@@ -39,6 +71,13 @@ def fetch_and_save_weather(request):
                     ny=item.get('ny', 0),
                 )
 
-            return render(request, 'success.html', {'message': 'Data fetched and saved successfully!', 'items': items})
+            
+            desired_categories = {'T1H', 'SKY', 'PTY'}
+            filtered_items = [item for item in items if item.get('category') in desired_categories]
+
+        # print(filtered_items) # 성공
+
+
+        return render(request, 'success.html', {'message': 'Data fetched and saved successfully!', 'filtered_items': filtered_items})
     
     return render(request, 'error.html', {'message': 'Failed to fetch data from the API.'})
