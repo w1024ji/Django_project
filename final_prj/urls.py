da29869f8cc21ba38c0b3240f9d2ad1750d60d28
장현지 <hy9458@duksung.ac.kr>
2023-11-27 15:31:13 +0900

    [1127]chat 기능

--- final_prj/urls.py
+++ final_prj/urls.py
@@ -21,6 +21,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('forecast/', include('forecast.urls')),
+    path("chat/", include("chat.urls")),
     # Add other app URLs as needed
 ]
 
