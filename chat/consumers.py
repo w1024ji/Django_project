8b0fd51957b71812aa312d348f8fb5e2cb7c0d8f
mogg22 <ahrthdus1@gmail.com>
2023-12-03 01:45:54 +0900

    [1203]chat 유저 정보 연결 및 mypage

--- chat/consumers.py
+++ chat/consumers.py
@@ -2,9 +2,9 @@
 import json
 
 from channels.generic.websocket import AsyncWebsocketConsumer
-
+from django.contrib.auth.models import User
 from channels.db import database_sync_to_async
-from .models import ChatMessage
+from .models import Chat, ChatRoom
 from django.utils import timezone
 
 
@@ -12,69 +12,127 @@ from django.utils import timezone
 
 class ChatConsumer(AsyncWebsocketConsumer):
     #db_create
-    @database_sync_to_async
-    def get_previous_messages(self):
-        return list(ChatMessage.objects.filter(room_name=self.room_name).values('message'))
+    # @database_sync_to_async
+    # def get_previous_messages(self):
+    #     return list(ChatMessage.objects.filter(room_name=self.room_name).values('message', 'user'))
 
     async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = f"chat_{self.room_name}"
+#        self.user = self.scope['user']
 
         # Join room group
-        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
 
         # Accept the WebSocket connection
         await self.accept()
 
-        #db_create
-        previous_messages = await self.get_previous_messages()
-        for message in previous_messages:
-            await self.send(
-                text_data=json.dumps({"message": f"{message['message']}"}))
-
+        # #db_create
+        # previous_messages = await self.get_previous_messages()
+        # for message in previous_messages:
+        #     await self.send(
+        #         text_data=json.dumps({"message": f"{message['message']}", "username": message['username']}))
+        #
+        # @database_sync_to_async
+        # def get_user(self, username):
+        #     return User.objects.get(username=username)
 
 
 
     async def disconnect(self, close_code):
         # Leave room group
-        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
 
    # Receive message from WebSocket
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
-     #   message_style = text_data_json.get("style", "")  # 추가된 부분
+        self.user_name = self.scope["user"].username
+        self.user_id = self.scope["user"].id
+        #username = text_data_json["username"]
 
-        # Send message to room group
-        # await self.channel_layer.group_send(
-        #     self.room_group_name, {"type": "chat.message", "message": message, "style": message_style}
-        # )
+        # user 생성
+       # username = text_data_json['username']
 
-        #db_create
-        # Save the message to the database
-        await self.save_message_to_database(message)
+     #   user = await self.get_user(username)
 
+        room = await database_sync_to_async(ChatRoom.objects.get)(name=self.room_name)
 
-
-        # Send message to room group
+        chat = Chat(
+            user=self.scope['user'],
+            message=message,
+            room=room,
+        )
+        await database_sync_to_async(chat.save)()
         await self.channel_layer.group_send(
-            self.room_group_name, {"type": "chat.message", "message": message, "timestamp": timezone.now().isoformat()}
-    #        self.room_group_name, {"type": "chat.message", "message": message}
+            self.room_group_name,
+            {
+                'type':'chat_message',
+                'message':message,
+                'user_id':self.user_id,
+                'user_name':self.user_name,
+            }
         )
 
-    @database_sync_to_async
-    def save_message_to_database(self, message):
-        ChatMessage.objects.create(room_name=self.room_name, message=message)
+        # db_create
+        # await self.save_message_to_database(message,  self.scope['user'])
 
+        # Save the message to the database
+    #    await self.save_message_to_database(message, self.scope['user'])
+        #        await self.save_message_to_database(message)
 
-
-    # Receive message from room group
     async def chat_message(self, event):
-        message = event["message"]
-        # Send message to WebSocket with HTML tags
+        message=event['message']
+        user_id=event['user_id']
+        user_name=event['user_name']
+
         await self.send(text_data=json.dumps({
-            "message": message,
+            'message':message,
+            'user_id':user_id,
+            'user_name': user_name
         }))
 
+
+
+
+
+
+        # Send message to room group
+    #     await self.channel_layer.group_send(
+    #         self.room_group_name, {"type": "chat.message", "message": message,
+    #                 "user_id": self.user_id, "user_name": self.user_name, "timestamp": timezone.now().isoformat()}
+    #         #"username": self.user.username,
+    # #        self.room_group_name, {"type": "chat.message", "message": message}
+    #     )
+
+#     @database_sync_to_async
+#     def save_message_to_database(self, message, username):
+#         ChatMessage.objects.create(
+#             room_name=self.room_name,
+#             message=message,
+#             username=self.user_name
+# #            username=self.user.username,
+#         )
+#
+#     # Receive message from room group
+#     async def chat_message(self, event):
+#         message = event["message"]
+#         user_id = event["user_id"]
+#         user_name = event["user_name"]
+#         # print(self.user.username)
+#         # print(user_name)
+#         # Send message to WebSocket with HTML tags
+#         await self.send(text_data=json.dumps({
+#             "message": message,
+#             "user_id": user_id,
+#             "user_name": user_name,
+#         }))
+#
 
 
